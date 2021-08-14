ddcba03fa7f59d340d5794ea56f64032fe3dce4e
Kingsley Quarshie <qkingsley436@gmail.com>
2021-08-14 01:45:12 +0000

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,21 @@
+// console.log("Hello world")
+
+// let a = 56; 
+
+// let b = 67;
+
+// const c = a + b;
+
+// console.log(c);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// add(1000, 35674)
+
+const add = (num1 , num2) => {
+    console.log(num1 + num2);
+}
+
+add(456, 908)
